45e60831c9a3358776ff87a550c8bd7d0444619b
Agustin Indarte <aindarte111@gmail.com>
2025-02-02 08:51:34 -0300

    Instalacion de Bootstrap React-Bootstrap React-Router, Defino la paginacion y el enrutamiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import "Bootstrap/dist/css/bootstrap.min.css"
+import { About, Contact, Home, Menu, NotFoundPage } from "./pages"
+import {Routes,Route  } from "react-router-dom";
+
 function App() {
 
   return (
     <>
-    
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/menu" element={<Menu/>} />
+        <Route path="/Abaut" element={<About/>} />
+        <Route path="/Contact" element={<Contact/>} />
+        
+        <Route path="*" element={<NotFoundPage/>} />
+        
+        
+      </Routes>
     </>
   )
 }
