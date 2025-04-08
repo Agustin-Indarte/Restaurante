0698fed3919b722a2b0226e68788c5e687d09b75
Agustin Indarte <aindarte111@gmail.com>
2025-04-07 23:28:24 -0300

    despliegue en vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "Bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { About, Contact, Home, Menu, NotFoundPage } from "./pages" /* Importe las paginas */
 import {Routes,Route  } from "react-router-dom"; /* Importo Routes para abrazar las rutas y Route para realizar el enrutamiento de cada una */
 import { Footer, NavBar } from "./components";
