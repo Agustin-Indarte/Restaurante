5cbb45fa4c26be96f127110cd7ac9a7328dc6e0c
Agustin Indarte <aindarte111@gmail.com>
2025-04-07 23:42:50 -0300

    Correcciones de los link del navbar en el despliegue

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,18 +8,19 @@ function NavBar() {
       <Container>
         <Navbar.Brand>
           <Link to="/" className='navbar-brand'>
-          <img src="LaLeñita_Logo.jpg" alt="Logo de La Leñita" className='img-fluid'/>
+            <img src="LaLeñita_Logo.jpg" alt="Logo de La Leñita" className='img-fluid' />
           </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto justify-content-end w-100 fw-bold" >
-            <Nav.Link href="/" className="active text-uppercase">Inicio</Nav.Link>
-            <Nav.Link href="/menu" className=" text-uppercase">Menu</Nav.Link>
-            <Nav.Link href="/about" className=" text-uppercase">Nosotros</Nav.Link>
-            <Nav.Link href="/contact" className=" text-uppercase">Contacto</Nav.Link>
+          <Nav className="me-auto justify-content-end w-100 fw-bold">
+            <Nav.Link as={Link} to="/" className="text-uppercase">Inicio</Nav.Link>
+            <Nav.Link as={Link} to="/menu" className="text-uppercase">Menu</Nav.Link>
+            <Nav.Link as={Link} to="/about" className="text-uppercase">Nosotros</Nav.Link>
+            <Nav.Link as={Link} to="/contact" className="text-uppercase">Contacto</Nav.Link>
           </Nav>
         </Navbar.Collapse>
+
 
       </Container>
     </Navbar>
